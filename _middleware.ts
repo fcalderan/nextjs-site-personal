1534fc544b603456ab193409b8a7f45914696de4
Fabrizio Calderan <fabrizio.calderan@akqa.com>
2023-03-31 23:15:26 +0200

    first release

---
+++ _middleware.ts
@@ -0,0 +1,25 @@
+// import { NextRequest, NextResponse } from "next/server";
+
+// export const config = {
+//   matcher: "/",
+// };
+
+// export function middleware(req: NextRequest) {
+//   const basicAuth = req.headers.get("authorization");
+//   const url = req.nextUrl;
+
+//   if (basicAuth) {
+//     const authValue = basicAuth.split(" ")[1];
+//     const [user, pwd] = atob(authValue).split(":");
+
+//     if (
+//       user === process.env.BASICAUTH_USER &&
+//       pwd === process.env.BASICAUTH_PASS
+//     ) {
+//       return NextResponse.next();
+//     }
+//   }
+//   url.pathname = "/api/basicauth";
+
+//   return NextResponse.rewrite(url);
+// }
